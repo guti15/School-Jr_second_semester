1b25ab1e2b31db961df591159e58a001c8edf39f
guti15 <guti310@gmail.com>
2014-04-16 13:31:08 -0400

    touch ups on py file small Algorithm

--- nlp_project idea/small Algorithm_solve_test.py
+++ nlp_project idea/small Algorithm_solve_test.py
@@ -8,25 +8,28 @@ NON_ENGLISH_STOPWORDS = set(nltk.corpus.stopwords.words()) - ENGLISH_STOPWORDS
 STOPWORDS_DICT = {lang: set(nltk.corpus.stopwords.words(lang)) for lang in nltk.corpus.stopwords.fileids()}
  
 
-
-# tell me the Languge 
-def get_language(text):
+def get_language(text):    # tell me the Languge 
     words = set(nltk.wordpunct_tokenize(text.lower()))
     return max(((lang, len(words & stopwords)) for lang, stopwords in STOPWORDS_DICT.items()), key = lambda x: x[1])[0]
+# end fuction 
 
-#is it an English Sentence
-def is_english(text):
+
+def is_english(text):     #is it an English Sentence
     text = text.lower()
     words = set(nltk.wordpunct_tokenize(text))
     return len(words & ENGLISH_STOPWORDS) > len(words & NON_ENGLISH_STOPWORDS)
+#end fuction 
 
-def split_count(sentence): 
+
+def split_count(sentence):     # slit the sentence and count where each words come from 
     languages_ratios = {}
-    split = wordpunct_tokenize(sentence)
-    words  = [word.lower()for word in split]
+    split = wordpunct_tokenize(sentence)  # tokenizes the input 
+    words  = [word.lower()for word in split]  # makes sentence lower in the list split 
+
     for language in stopwords.fileids(): 
-        stopwords_set = set(stopwords.words(language))
-        words_set = set(words) 
+        stopwords_set = set(stopwords.words(language))  #creats a set of language based on the token lang.
+
+        words_set = set(words)  # creates a set of words 
         common_element = words_set.intersection(stopwords_set)
         languages_ratios[language] = len(common_element) # this will detrmain the score
     print languages_ratios
@@ -50,11 +53,20 @@ def main():
 
     simple_Sent ="Hola como estas. Espero que eses bien"
     
-    print "\n"
-
+    print "\n spanish"
+    # ---- (count the tokens) 
     split_count(sentence_test_spanish)
-    
+    print "\n english"
+    split_count(sentence_test_english)
+    print "\n french"
+    split_count(Sentence_test_french)
+    print "\n sweedish"
+    split_count(Sentence_test_sweedish)
+    # end count  of tokens 
 
+    # PRINT  what language is the sentence 
+    # PRINT  is it ENGLISH  (true or false)
+    
     print "\n"
     # ------- PRINT THE SPANISH VERSION SENTENCE 
     print get_language(sentence_test_spanish)  # Gives language 
